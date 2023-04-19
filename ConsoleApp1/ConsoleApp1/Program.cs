59ae6e2388945d1713c2a74e3b98b843cbff0e35
kalininmihail <114031757+kalininmihail@users.noreply.github.com>
2023-04-19 10:51:05 +0300

    Create Text Output Structure

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    class Cheese
+    {
+        public string Chs_Brand;
+        public string Chs_Name;
+        public int Chs_Fat;
+    
+
+
+        // Создаем параметрический конструктор
+        public Cheese (string chs_brand, string chs_name, int chs_fat)
+        {
+            Chs_Brand = chs_brand;
+            Chs_Name = chs_name;
+            Chs_Fat = chs_fat;
+        }
+
+        public void reWrite()
+        {
+            Console.WriteLine("Производитель {0}\nИмя: {1}\nПроцент жира: {2}", Chs_Brand, Chs_Name, Chs_Fat);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Cheese ex = new Cheese ("RosMoloko","Almette", 40);
+            ex.reWrite();
 
-
+            Console.ReadLine();
         }
     }
 }
